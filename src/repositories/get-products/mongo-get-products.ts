7659e3696247c14b7cd9f6fe80af5116d97b91a0
yalves8 <yasminleticia_@hotmail.com>
2024-03-16 00:25:17 -0300

    feat: GetProducts from Mongo

--- src/repositories/get-products/mongo-get-products.ts
+++ src/repositories/get-products/mongo-get-products.ts
@@ -1,15 +1,17 @@
 import { IGetProductsRepository } from "../../controllers/get-products/protocols";
+import { MongoClient } from "../../database/mongo";
 import { Product } from "../../models/product";
 
 export class MongoGetProductsRepository implements IGetProductsRepository {
   async getProducts(): Promise<Product[]> {
-    return [
-      {
-        id: 1,
-        name: "Yasmin",
-        preco: 50,
-      },
-    ];
+    const products = await MongoClient.db
+      .collection<Omit<Product, "id">>("produtos")
+      .find({})
+      .toArray();
+    return products.map(({ _id, ...rest }) => ({
+      ...rest,
+      id: _id.toHexString(),
+    }));
   }
 }
 
